26341149273f17956efcecff9ae1d969b50c5aff
sydney_p <sydneypapier23@gmail.com>
2025-05-13 09:17:53 +0200

    Added files

---
+++ TicTacToeSubmissionSln/TicTacToeSubmissionConole/TicTacToe.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TicTacToeRendererLib.Enums;
+using TicTacToeRendererLib.Renderer;
+
+namespace TicTacToeSubmissionConole
+{
+    public class TicTacToe
+    {
+        private TicTacToeConsoleRenderer _boardRenderer;
+
+        public TicTacToe()
+        {
+            _boardRenderer = new TicTacToeConsoleRenderer(10,6);
+            _boardRenderer.Render();
+        }
+
+
+        public void Run()
+        {
+
+            // FOR ILLUSTRATION CHANGE TO YOUR OWN LOGIC TO DO TIC TAC TOE
+            //Player X
+            Console.SetCursorPosition(2, 19);
+
+            Console.Write("Player X");
+
+            Console.SetCursorPosition(2, 20);
+
+            Console.Write("Please Enter Row: ");
+            var row = Console.ReadLine();
+
+            Console.SetCursorPosition(2, 22);
+
+
+            Console.Write("Please Enter Column: ");
+            var column = Console.ReadLine();
+
+            //player O
+            Console.SetCursorPosition(2, 19);
+
+            Console.Write("Player O");
+
+            Console.SetCursorPosition(2, 20);
+
+            Console.Write("Please Enter Row: ");
+            var row = Console.ReadLine();
+
+            Console.SetCursorPosition(2, 22);
+
+            Console.Write("Please Enter Column: ");
+            var column = Console.ReadLine();
+
+
+
+            // THIS JUST DRAWS THE BOARD (NO TIC TAC TOE LOGIC)
+            _boardRenderer.AddMove(int.Parse(row), int.Parse(column), PlayerEnum.X, true);            
+
+        }
+
+    }
+}
